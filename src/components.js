558903d94d608584a8dab37457816fcac39f8206
wonseok-do <wsdo970@gmail.com>
2022-08-07 00:16:54 +0900

    text,input,button추가

---
+++ src/components.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const TextArea = ({ text }) => {
+  return (
+    <div style={{ width: "50vw", minHeight: "20vh", border: "1px solid #888" }}>
+      <pre>{text}</pre>
+    </div>
+  );
+};
+
+export const Input = ({ input_ref }) => {
+  return <input ref={input_ref} />;
+};
+
+export const Button = ({ input_ref, setText }) => {
+  return (
+    <button
+      onClick={() => {
+        console.log(input_ref.current.value);
+        setText(input_ref.current.value);
+        input_ref.current.value = "";
+      }}
+    >
+      완성
+    </button>
+  );
+};
