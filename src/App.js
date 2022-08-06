234ea2af779cfef74bdb03bfd777ef5821c6ac99
wonseok-do <wsdo970@gmail.com>
2022-08-07 00:34:55 +0900

    useCompletes 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { Button, Input, TextArea } from "./components";
+import useCompletes from "./useCompletes";
 
 function App() {
   const input_ref = React.useRef(null);
-  const [text, setText] = React.useState("");
+  // const [text, setText] = React.useState("");
+  const [text, setText] = useCompletes("");
   return (
     <div className="App" style={{ display: "flex", gap: 10 }}>
       <div>
