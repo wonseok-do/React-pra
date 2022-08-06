89f1262745551d87939a8f56e5cf5543a00409e4
wonseok-do <wsdo970@gmail.com>
2022-08-07 00:18:43 +0900

    useRef,useState 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Button, Input, TextArea } from "./components";
 
 function App() {
+  const input_ref = React.useRef(null);
+  const [text, setText] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", gap: 10 }}>
+      <div>
+        <TextArea text={text} />
+      </div>
+      <div>
+        <Input input_ref={input_ref} />
+        <Button input_ref={input_ref} setText={setText} />
+      </div>
     </div>
   );
 }
